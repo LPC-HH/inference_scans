1cddac140475e0247040f7aa5e3722e46f102c79
Javier <jduarte@caltech.edu>
2022-02-26 04:16:33 +0100

    upper limits at point

---
+++ run_upper_limits_at_point.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+law run PlotUpperLimitsAtPoint \
+    --hh-model "$MODEL_BOOSTED" \
+    --version "$VERSION" \
+    --multi-datacards "$MULTICARDS" \
+    --file-types "pdf,png" \
+    --pois r \
+    --UpperLimits-workflow "htcondor" \
+    --UpperLimits-tasks-per-job 1 \
+    --datacard-names "$MULTINAMES" \
+    --x-log \
+    --campaign run2 \
+    --workers 8 \
+    --use-snapshot False \
+    --unblinded True \
+    --remove-output 10,a,y \
+    --datacard-order 4,3,2,1,0
