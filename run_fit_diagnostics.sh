136a3d757ca44af2ceec218a34043abcd0a9dc1e
Javier Duarte <jduarte@ucsd.edu>
2022-03-07 07:56:27 +0100

    add b-only fit

--- run_fit_diagnostics.sh
+++ run_fit_diagnostics.sh
@@ -4,7 +4,17 @@ law run FitDiagnostics \
     --hh-model "$MODEL_BOOSTED_CLOSURE" \
     --version "$VERSION" \
     --datacards "$C4bcomb" \
+    --skip-b-only False \
     --pois r \
     --use-snapshot True \
-    --unblinded True \
-    --remove-output 0,a,y
+    --unblinded True
+
+law run FitDiagnostics \
+    --hh-model "$MODEL_BOOSTED_CLOSURE" \
+    --version "$VERSION" \
+    --datacards "$C4bcomb" \
+    --skip-b-only False \
+    --pois r \
+    --parameter-values C2V=0 \
+    --use-snapshot True \
+    --unblinded True
